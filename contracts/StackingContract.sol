cbf2216ba9e1ea53d62f1a4e5a8c26bb21adbc50
The-3D <emilio@ethlend.io>
2018-08-08 09:39:21 +0200

    Initial commit

--- contracts/StackingContract.sol
+++ contracts/StackingContract.sol
@@ -9,20 +9,31 @@ contract StakingContract {
 using SafeMath for uint96;
 using SafeMath for uint;
  
+ uint constant  REWARD_PERCENTAGE = 5;
+ uint constant  WITHDRAWING_WHILE_ACTIVE_PENALTY = 20;
  
  struct UserStakeData{
     
     uint96 currentStakeAmount;
-    uint96 sentAmount;
+    uint96 totalSentAmount;
     uint64 stakeUnlockDate;
+    uint32 lastRewardRoundWithdrawn;
+    mapping(uint32 => uint96) amountsHistory;
  }
-
- constant REWARD_PERCENTAGE = 5;
- constant WITHDRAWING_WHILE_ACTIVE_PENALTY = 20;
  
- mapping (address => UserStakeData) userStakes ;
- address[] public userStakesAddresses;
+ struct RewardData{
+     
+     uint totalReward;
+     uint totalEthReceived;
+     uint rewardDate;
+ }
+ 
+ mapping (address => UserStakeData) userStakes;
 
+ mapping (uint => RewardData) rewards;
+
+ uint32 rewardRoundNumber = 0;
+ 
  uint public totalEthReceived = 0;
  
  uint public totalTokenLockedAmount = 0;
@@ -31,15 +42,13 @@ using SafeMath for uint;
   
  uint public lastRewardDistributionDate;
 
-
  ERC20 public token ;
   
  KyberNetworkProxy public kyberContract ;
   
- 
-   
 
-function StakingContract(address _tokenAddress, address _kyberContract) public {
+constructor(address _tokenAddress, address _kyberContract) public {
+
         // Check inputs
         require(_tokenAddress != address(0));
         require(_kyberContract != address(0));
@@ -49,37 +58,46 @@ function StakingContract(address _tokenAddress, address _kyberContract) public {
     
 }
 
- function getStakesAddresses() public view returns (address[]) {
+
+ function claimRewards() public{
      
-     return userStakesAddresses;
- }
- 
- function getUserStakeData() public view returns (uint96 stake, uint96 total, uint64 unlockDate){
- 
-    UserStakeData storage stakeData = userStakes[msg.sender];
-    
-    stake = stakeData.currentStakeAmount;
-    total = stakeData.sentAmount;
-    unlockDate = stakeData.stakeUnlockDate;
+     UserStakeData storage data = userStakes[msg.sender];
+
+     for(uint32 i=data.lastRewardRoundWithdrawn; i < rewardRoundNumber; i++ ){
+         
+         uint96 totalPaidForRound = data.amountsHistory[i];
+         
+         RewardData dataForRound = rewards[i];
+         
+         uint reward = totalPaidForRound.div(dataForRound.totalEthReceived).mul(dataForRound.totalReward);
+         
+         data.currentStakeAmount += uint96(reward);
+     }
+     
+     data.lastRewardRoundWithdrawn = rewardRoundNumber;
      
  }
  
  function withdrawStake() public {
      
-     UserStakeData storage stakeData = UserStakes[msg.sender];
      
-     require(stakeData.currentStakeAmount > 0);
+
+    UserStakeData storage stakeData = userStakes[msg.sender];
      
-     token.approve(this, stakeData.currentStakeAmount);
+    require(stakeData.currentStakeAmount > 0);
      
-     var amountToWitdraw = stakeData.currentStakeAmount;
+    token.approve(this, stakeData.currentStakeAmount);
      
-     if(stakeData.stakeUnlockDate >= now)
-        amountToWitdraw  = amountToWitdraw - amountToWitdraw.mul(WITHDRAWING_WHILE_ACTIVE_PENALTY).div(100);
-     token.transferFrom(this, msg.sender, amountToWitdraw);
+    uint amountToWitdraw = stakeData.currentStakeAmount;
      
-     stakeData.currentStakeAmount = 
+    if(stakeData.stakeUnlockDate >= now)
+        amountToWitdraw  = amountToWitdraw.sub(amountToWitdraw.mul(WITHDRAWING_WHILE_ACTIVE_PENALTY).div(100));
+    
+    token.transferFrom(this, msg.sender, amountToWitdraw);
      
+    totalTokenLockedAmount.sub(stakeData.currentStakeAmount);
+    stakeData.currentStakeAmount = 0;
+    stakeData.totalSentAmount = 0;
  }
 
  
@@ -89,59 +107,49 @@ function StakingContract(address _tokenAddress, address _kyberContract) public {
     require(msg.value > 0);
     
     UserStakeData storage data = userStakes[msg.sender];
-    
-    if(data.sentAmount == 0)
-        userStakesAddresses.push(msg.sender);
 
-    data.sentAmount =data.sentAmount + uint96(msg.value);
+    data.totalSentAmount += uint96(msg.value);
+    data.amountsHistory[rewardRoundNumber] =  data.totalSentAmount;
+
     data.stakeUnlockDate = uint64(now);
  
  }
  
- function calculateGlobalStackSize() view public returns(uint size){
+ function calculateGlobalStakeSize() view public returns(uint size){
      
      size = token.balanceOf(this) - totalTokenLockedAmount;
  }
  
  
- function calculateRewards() public {
+ function generateReward() public {
      
-    var currentGlobalStakeSize = calculateGlobalStackSize();
+    var currentGlobalStakeSize = calculateGlobalStakeSize();
     
-    var currentRewardAmount = currentGlobalStakeSize.mul(REWARD_PERCENTAGE).div(100);
+    var rewardAmount = currentGlobalStakeSize.mul(REWARD_PERCENTAGE).div(100);
     
-    for(var i =0; i<userStakesAddresses.length; i++){
-        
-        UserStakeData storage data = userStakes[userStakesAddresses[i]];
-        
-        var ethPaidPercentage = totalEthReceived.div(data.sentAmount);
-        
-        var amountToStake = currentRewardAmount.mul(ethPaidPercentage);
-        
-        currentRewardAmount -= amountToStake;
-        
-        data.currentStakeAmount += uint96(amountToStake);
-        totalTokenLockedAmount += amountToStake;
-        
-    }
+    RewardData storage rewardData = rewards[rewardRoundNumber];
     
-    require(currentRewardAmount == 0);
+    rewardData.totalReward = rewardAmount;
+    rewardData.totalEthReceived = totalEthReceived;
+    rewardData.rewardDate = now;
     
-     
+    totalTokenLockedAmount += rewardAmount;
+    
+    ++rewardRoundNumber;
  } 
  
 
  function convert() public{
 
-        totalEthReceived=totalEthReceived+this.balance;
+    totalEthReceived=totalEthReceived+this.balance;
 
-        uint ethToConvert = this.balance;
+    uint ethToConvert = this.balance;
    
-        uint maxDestAmount = 2**256 - 1;
+    uint maxDestAmount = 2**256 - 1;
 
-        // Set minConversionRate to 1 
-        // A value of 1 will execute the trade according to market price in the time of the transaction confirmation
-        uint minConversionRate =   1;
+    // Set minConversionRate to 1 
+    // A value of 1 will execute the trade according to market price in the time of the transaction confirmation
+    uint minConversionRate =   1;
 
 
         // Convert the ETH to ERC20
@@ -171,6 +179,55 @@ function StakingContract(address _tokenAddress, address _kyberContract) public {
 
  }
  
+ 
+ function getUnclaimedRewards() public view returns(uint total, uint numOfUnpaidRewards){
+     
+         
+     UserStakeData storage data = userStakes[msg.sender];
+     
+     
+
+     for(uint32 i=data.lastRewardRoundWithdrawn; i < rewardRoundNumber; i++ ){
+         
+         uint96 totalPaidForRound = data.amountsHistory[i];
+         
+         RewardData dataForRound = rewards[i];
+         
+         uint reward = totalPaidForRound.div(dataForRound.totalEthReceived).mul(dataForRound.totalReward);
+         
+         total+=reward;
+     }
+     
+     numOfUnpaidRewards = rewardRoundNumber-data.lastRewardRoundWithdrawn;
+ }
+ 
+ 
+ function getCurrentRewardRound() public view returns(uint32){
+     
+     return rewardRoundNumber;
+ }
+
+  
+   
+function getTotalRewardsForRound(uint32 index) public view returns (uint rewardAmount, uint totalEthReceived){
+    
+    require(index < rewardRoundNumber);
+    
+    RewardData storage data = rewards[index];
+    
+    rewardAmount = data.totalReward;
+    totalEthReceived = data.totalEthReceived;
+}
+
+ function getUserStakeData() public view returns (uint96 stake, uint96 total, uint64 unlockDate){
+ 
+    UserStakeData storage stakeData = userStakes[msg.sender];
+    
+    stake = stakeData.currentStakeAmount;
+    total = stakeData.totalSentAmount;
+    unlockDate = stakeData.stakeUnlockDate;
+     
+ }
  
  
  /*
