c4d22f0c5fae6bed89689d13fcb5f67251e583ad
The-3D <37953689+The-3D@users.noreply.github.com>
2018-08-22 22:43:59 +0200

    updated migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var token = artifacts.require("MockERC20");
 var tokenTrader = artifacts.require("MockTokenTrader");
 var stakingContract = artifacts.require("StakingContract");
+var stakingLibrary = artifacts.require("StakingLibrary");
 
 module.exports = async function (deployer) {
 
@@ -10,7 +11,13 @@ module.exports = async function (deployer) {
 
     console.log("Deploying staking smart contract, token address: "+token.address+", trader address: "+tokenTrader.address );
 
+
+    await deployer.deploy(stakingLibrary);
+
+    deployer.link(stakingLibrary, stakingContract);
+
     await deployer.deploy(stakingContract, token.address, tokenTrader.address);
 
+    console.log("Staking smart contract deployed, address: "+stakingContract.address );
 
 };
